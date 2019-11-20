19b89a615f64b517f5ee5a434ce06a06f6988bb2
Johanna Reisswig <johannareisswig@gmail.com>
2019-11-20 14:45:51 +0100

    Resolved BookmarkFilter

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import React, { useState } from 'react'
 import cardData from './cards.json'
-
+import FilterBookmark from './FilterBookmark'
 import Card from './Card'
 import styled from 'styled-components/macro'
 
-const CardContainer = styled.div`
-  display: grid;
-  gap: 20px;
-  padding: 20px;
-  overflow: scroll;
-`
-
 function App() {
   const [cards, setCards] = useState(cardData)
+  const [isOnlyBookmarksShown, setisOnlyBookmarksShown] = useState(false)
 
   return (
     <div>
-      <CardContainer>
-        {cards.map((card, index) => (
-          <Card
-            question={card.question}
-            answer={card.answer}
-            key={index}
-            isBookmarked={card.isBookmarked}
-            toggleBookmarked={() => toggleBookmarked(index)}
-          />
-        ))}
-      </CardContainer>
+      <FilterBookmark onClick={filterBookmarkedCards}>
+        {isOnlyBookmarksShown ? 'Show bookmarked cards' : 'Show all Cards'}{' '}
+      </FilterBookmark>
+      {!isOnlyBookmarksShown ? (
+        <CardContainer>
+          {cards.map((card, index) => (
+            <Card
+              question={card.question}
+              answer={card.answer}
+              key={index}
+              isBookmarked={card.isBookmarked}
+              toggleBookmarked={() => toggleBookmarked(index)}
+            />
+          ))}
+        </CardContainer>
+      ) : (
+        <CardContainer>
+          {cards
+            .filter(card => card.isBookmarked === true)
+            .map((card, index) => (
+              <Card
+                question={card.question}
+                answer={card.answer}
+                key={index}
+                isBookmarked={card.isBookmarked}
+                toggleBookmarked={() => toggleBookmarked(index)}
+              />
+            ))}
+        </CardContainer>
+      )}
     </div>
   )
+
+  function filterBookmarkedCards() {
+    setisOnlyBookmarksShown(!isOnlyBookmarksShown)
+  }
 
   function toggleBookmarked(index) {
     const card = cards[index]
@@ -39,5 +56,12 @@ function App() {
     ])
   }
 }
+
+const CardContainer = styled.div`
+  display: grid;
+  gap: 20px;
+  padding: 20px;
+  overflow: scroll;
+`
 
 export default App
---
+++ src/FilterBookmark.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/macro'
+
+export default styled.button`
+  background-color: lightgrey;
+  border: none;
+  padding: 5px;
+`
